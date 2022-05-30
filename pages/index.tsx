5f3470e4a3cc836d52189bf6cddd8261f6b3eb8b
HYUCK7 <iqeq12191219@gmail.com>
2022-05-30 16:03:33 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { Counter } from '@/components/counter/Counter'
+import type { NextPage } from 'next'
+import styles from '@/styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <Counter/>
+  )
+}
+
+export default Home
